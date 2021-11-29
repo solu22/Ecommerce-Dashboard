b7788dea53102566e0e3945c283c44becb4007b1
Pasang Nuru Sherpa <79486506+solu22@users.noreply.github.com>
2021-11-29 02:56:23 +0200

    database setup for products and route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,5 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', [UserController:: class, 'register']);
-Route::post('/login', [UserController:: class, 'login']);
+Route::post('register', [UserController:: class, 'register']);
+Route::post('login', [UserController:: class, 'login']);
+Route::post('addproduct', [ProductController:: class, 'addProduct']);
+
